d787398cbe4adf9f1b156c00dc602f2fc2135b12
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-01 05:34:48 -0700

    Reverted changes to use a client connection before using app.listen()

--- server.js
+++ server.js
@@ -3,20 +3,20 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
-const client = require(`${__dirname}/src/config/databaseConfig`);
+// const client = require(`${__dirname}/src/config/databaseConfig`);
 
 const app = require(`${__dirname}/src/middleware/app`);
 
 const port = process.env.PORT || 5500;
 
-client.connect(err => {
-    if(err){ console.error(err); return false;}
-    // connection to mongo is successful, listen for requests
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-});
+// client.connect(err => {
+//     if(err){ console.error(err); return false;}
+//     // connection to mongo is successful, listen for requests
+//     app.listen(PORT, () => {
+//         console.log("listening for requests");
+//     })
+// });
 
-// app.listen(port, () => {
-//     console.log(`Listening to port ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
