57dc1ad21c3a3ed03353152c5719eec9fd00e4e6
Isaiah Youm <isaiahyoum.coding@gmail.com>
2023-05-01 05:13:39 -0700

    Connected client to serverless app hosting.

--- server.js
+++ server.js
@@ -3,10 +3,20 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
+const client = require(`${__dirname}/../config/databaseConfig`);
+
 const app = require(`${__dirname}/src/middleware/app`);
 
 const port = process.env.PORT || 5500;
 
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
-})
+client.connect(err => {
+    if(err){ console.error(err); return false;}
+    // connection to mongo is successful, listen for requests
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+});
+
+// app.listen(port, () => {
+//     console.log(`Listening to port ${port}`);
+// })
